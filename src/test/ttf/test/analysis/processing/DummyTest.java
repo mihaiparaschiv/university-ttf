f79e6fd2cf30edc110e840e764e16c7904326104
mihai.paraschiv <mihai.paraschiv@232b5d9a-6c97-ffb4-081d-cb002e1f0bcf>
2010-04-27 10:22:50 +0000

    Refactored the dummy task test.
    git-svn-id: http://topic-tracking-framework.googlecode.com/svn/ttf/trunk@20 232b5d9a-6c97-ffb4-081d-cb002e1f0bcf

--- src/test/ttf/test/analysis/processing/DummyTest.java
+++ src/test/ttf/test/analysis/processing/DummyTest.java
@@ -1,12 +1,16 @@
 package ttf.test.analysis.processing;
 
+import static junit.framework.Assert.*;
+
 import java.util.Date;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import ttf.analysis.AnalysisController;
+import ttf.analysis.processing.Processor;
 import ttf.analysis.processing.ProcessorFactory;
+import ttf.analysis.processing.ProcessorStore;
+import ttf.analysis.processing.task.Task;
 import ttf.model.entry.Entry;
 import ttf.model.entry.EntryFactory;
 import ttf.model.property.value.AddressValue;
@@ -15,25 +19,47 @@ import ttf.model.property.value.TextValue;
 import ttf.test.analysis.LinkedListProvider;
 
 public class DummyTest {
-	private AnalysisController analysisController;
-	
-	@Before
-	public void beforeClass() {
+	private static final String NEW_NAME = "new name";
+
+	@Test
+	public void testDummyTask() {
 		EntryFactory factory = new EntryFactory();
 		Entry entry = factory.build();
 		entry.getAddress().setValue(new AddressValue("http://..."));
 		entry.getName().setValue(new TextValue("A news article"));
 		entry.getContent().setValue(new TextValue("News content"));
 		entry.getDiscoveredAt().setValue(new DateValue(new Date()));
-		
+
 		LinkedListProvider entryProvider = new LinkedListProvider();
 		entryProvider.add(entry);
 		ProcessorFactory processorFactory = new DummyProcessorFactory();
-		analysisController = new AnalysisController(entryProvider, processorFactory);
+		AnalysisController controller = new AnalysisController(entryProvider,
+				processorFactory);
+		controller.execute();
+		assertEquals(NEW_NAME, entry.getName().getValue().get());
 	}
-	
-	@Test
-	public void testDummyTask() {
-		analysisController.execute();
+
+	private class DummyTask extends Task {
+		public DummyTask(Processor processor) {
+			super(processor);
+		}
+
+		@Override
+		public void execute() {
+			Entry entry = processor.getStore().getEntry();
+			System.out.println(entry);
+			entry.getName().setValue(new TextValue(NEW_NAME));
+		}
+	}
+
+	private class DummyProcessorFactory implements ProcessorFactory {
+		@Override
+		public Processor build(Entry entry) {
+			ProcessorStore store = new ProcessorStore();
+			store.setEntry(entry);
+			Processor processor = new Processor(store);
+			processor.addTask(new DummyTask(processor));
+			return processor;
+		}
 	}
 }
